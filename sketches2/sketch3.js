4e9a3a5f36097cc7aa5dac3087dab7d8a0986bc0
katealexandrite <katherine.guillen@duke.edu>
2022-09-12 07:46:40 -0400

    Update to Assignments

--- sketches2/sketch3.js
+++ sketches2/sketch3.js
@@ -1,30 +1,28 @@
-let t = 0; // time variable
+let dim;
 
 function setup() {
-  createCanvas(2000, 200);
+  createCanvas(1600, 100);
+  dim = width / 16;
+  background(0);
+  colorMode(HSB, 360, 50, 100);
   noStroke();
-  fill(155, 100, 180);
+  ellipseMode(RADIUS);
+  frameRate(.2);
 }
 
 function draw() {
-  background(10, 20); // translucent background (creates trails)
-
-  // make a x and y grid of ellipses
-  for (let x = 0; x <= width; x = x + 30) {
-    for (let y = 0; y <= height; y = y + 30) {
-      // starting point of each circle depends on mouse position
-      const xAngle = map(mouseX, -15, width, 1 * PI, 15 * PI, true);
-      const yAngle = map(mouseY, -15, height, 1 * PI, 15 * PI, true);
-      // and also varies based on the particle's location
-      const angle = xAngle * (x / width) + yAngle * (y / height);
-
-      // each particle moves in a circle
-      const myX = x * cos(2 * PI * t + angle);
-      const myY = y * sin(2 * PI * t + angle);
-
-      ellipse(myX, myY, 5); // draw particle
-    }
+  background(0);
+  for (let x = 0; x <= width; x += dim) {
+    drawGradient(x, height / 4);
   }
+}
 
-  t = t + 0.02; // update time
+function drawGradient(x, y) {
+  let radius = dim /1;
+  let h = random(0, 360);
+  for (let r = radius; r > 0; --r) {
+    fill(h, 90, 90);
+    ellipse(x, y, r, r);
+    h = (h + 1) % 360;
+  }
 }
